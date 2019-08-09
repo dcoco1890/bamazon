45c0f3f508adc7dcefed248a2399d064ef4597af
Cohen <dcoco91@gmail.com>
2019-08-09 15:02:10 -0400

    made all the logic to show customer what they ordered, and set up a way to make multiple orders

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,14 +11,32 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+var endQuestion = [{
+    name: "escape",
+    type: "confirm",
+    message: "Order again?",
+    default: true
+}];
+
 connection.connect(function(err) {
     if (err) throw err;
     start();
 });
 
+function start() {
+    connection.query(`SELECT product_name, department from products`, function(err, results) {
+        if (err) throw err;
+        makeTable(results);
+        display();
+
+    });
+
+}
+
+//this is used just to display the items to the user
 function makeTable(info) {
 
-    var c = 1;
+    var c = 1; //keeps track of, and numbers the items
     console.log(`\n\n`);
     console.log(`----------Here's what's available----------\n`);
     for (var i = 0; i < info.length; i++) {
@@ -33,41 +51,65 @@ function makeTable(info) {
 
 function display() {
 
-    ui.log.write("Pick a number, guy".green);
-    inquirer.prompt([{
-            name: "item",
-            message: "What item number would you like to purchase?",
-            type: "input",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        },
-        {
-            name: "howmany",
-            message: "How many would you like to purchase?",
-            type: "input",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        }
-    ]).then(ans => {
+    var q = `SELECT * FROM products`;
+    connection.query(q, function(err, res) {
 
-        console.log(ans.item);
-    });
-}
-
-function start() {
-    connection.query(`SELECT product_name, department from products`, function(err, results) {
         if (err) throw err;
-        makeTable(results);
-        display();
+        var info = res;
+        ui.log.write("Pick a number, guy".green);
+        inquirer.prompt([{
+                name: "item",
+                message: "What item number would you like to purchase?",
+                type: "input",
+                validate: function(value) {
+                    if (isNaN(value) === false && value <= info.length && value > 0) {
+                        return true;
+                    }
+                    return "That's not a product number";
+                }
+            },
+            {
+                name: "howmany",
+                message: "How many would you like to purchase?",
+                type: "input",
+                validate: function(value) {
+                    if (isNaN(value) === false && value > 0) {
+                        return true;
+                    }
+                    return "C'mon guy enter a real number";
+                }
+            }
+        ]).then(ans => {
+            var item = ans.item;
+            var amount = ans.howmany;
+            var x = info[item - 1].stock;
 
-    });
+            if (amount <= x) {
+                console.log(`\nPlacing your order for`);
+                console.log(`${amount}  of this:  ${info[item - 1].product_name}`.green);
+                console.log(`---------------------`);
+                console.log(`${info[item - 1].price} * ${amount}`.red);
+                console.log(`You owe ${amount * info[item - 1].price}`);
+                console.log(`---------------------\n`);
+                // updateStore(item, amount);
+            } else {
+                console.log(`Sorry, we don't have enough of that!`);
+            }
+
+            inquirer.prompt(endQuestion).then(ans => {
+                if (ans.escape) {
+                    start();
+                } else {
+                    connection.end();
+                };
+            });
+
+
+        });
+
+    })
+};
+
+function updateStore(product, amount) {
 
 }
